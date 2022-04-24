b01f746b304f849f040273b716e202f9044aba10
Thanh <lethanhhhhha@gmail.com>
2022-04-24 20:56:43 +0700

    update real url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -3,18 +3,20 @@ import queryString from "query-string";
 
 // mainUrl:"https://ecommerce-lethanh.herokuapp.com"
 //subUrl :"http://localhost:5000"
- const instance = axios.create({
-    baseURL: "http://localhost:5000",
-    paramsSerializer: (params) => {
-      return queryString.stringify({ ...params });
-    },
-  });
-  
-export default instance
-
-instance.interceptors.response.use(function (response) {
-  return response.data;
-}, function (error) {
-  
-  return Promise.reject(error);
+const instance = axios.create({
+  baseURL: "https://ecommerce-lethanh.herokuapp.com",
+  paramsSerializer: (params) => {
+    return queryString.stringify({ ...params });
+  },
 });
+
+export default instance;
+
+instance.interceptors.response.use(
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
